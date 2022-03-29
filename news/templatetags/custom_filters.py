13c04c602a7a63d33bdf65f4a863583744bb4aed
maxnewton <newton0512@gmail.com>
2022-03-29 20:42:28 +0300

    Добавлен декоратор @stringfilter

--- news/templatetags/custom_filters.py
+++ news/templatetags/custom_filters.py
@@ -1,6 +1,8 @@
 from django import template
 import re
 
+from django.template.defaultfilters import stringfilter
+
 register = template.Library()
 
 bad_words = ['очень', 'плохие', 'слова', 'что', 'для']
@@ -15,5 +17,6 @@ def replace(match):
 
 
 @register.filter()
+@stringfilter
 def censor(value):
     return re.sub(r'\b\w*\b', replace, value, flags=re.I | re.U)
